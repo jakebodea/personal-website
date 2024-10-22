f612556c1989d8744a10bfa6c97432f8cf30e275
Jake Bodea <jakebodea@gmail.com>
2024-10-22 11:07:52 -0700

    trying again...

--- types/lucide-react.d.ts
+++
@@ -1,6 +0,0 @@
-declare module 'lucide-react' {
-  export const Moon: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const Sun: React.FC<React.SVGProps<SVGSVGElement>>;
-  export const ArrowUpRight: React.FC<React.SVGProps<SVGSVGElement>>;
-  // Add other icons as needed
-}
