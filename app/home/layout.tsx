31fe615a734926cb68de5620086a59df4d7e77f9
jakebodea <jakebodea@gmail.com>
2025-06-13 21:03:54 -0700

    home page annimation with framer motion

---
+++ app/home/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Home',
+  description: 'Jake Bodea\'s home page'
+}
+
+export default function HomeLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children;
+} 
