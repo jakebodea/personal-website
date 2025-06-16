050e36a7ee8025fd1b44caf274d8c165b0bd85b5
Jake Bodea <jake.bodea@taxrise.com>
2025-06-16 12:16:59 -0700

    added new quote

--- content/quotes-data.ts
+++ content/quotes-data.ts
@@ -10,5 +10,9 @@ export const quotesData = [
   {
     "quote": "You need to be proud of your work, and your work needs to be worthy of pride",
     "author": "Mr. Mayfield (8th grade history)"
+  }, 
+  {
+    "quote": "Just make it exist first, you can make it good later.",
+    "author": "idk"
   }
 ] 
